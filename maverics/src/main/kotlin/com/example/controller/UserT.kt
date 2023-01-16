030e7713cbab1fc00f60764bd6952c59f2734e33
Vishal Borse <vishalb@Vishals-MacBook-Pro.local>
2023-01-17 00:20:52 +0530

    Add partial reg route

--- maverics/src/main/kotlin/com/example/controller/UserT.kt
+++ maverics/src/main/kotlin/com/example/controller/UserT.kt
@@ -1,8 +1,13 @@
 package com.example.controller
 
+import com.example.model.User
+import com.fasterxml.jackson.annotation.JsonAlias
+import com.fasterxml.jackson.databind.util.JSONPObject
+import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Post
+import io.micronaut.json.tree.JsonObject
 
 
 @Controller("/user")
@@ -11,6 +16,14 @@ class UserT {
     @Get
     fun test(): String{
         return "sucess";
+    }
+
+    @Post("/register")
+    fun register(@Body body:JsonObject):User{
+
+        var newUser=User()
+        newUser.firstName=body["firstName"].stringValue
+        return newUser
     }
 
 
