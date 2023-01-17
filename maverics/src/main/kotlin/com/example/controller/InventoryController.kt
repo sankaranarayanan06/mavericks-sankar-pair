07cfc4276548ef0f6e5fcad0f6d05774b0a76353
Pranav-Bhosale <pranav.b.bhosale@gmail.com>
2023-01-17 13:29:05 +0530

    add inventory controller

--- maverics/src/main/kotlin/com/example/controller/InventoryController.kt
+++ maverics/src/main/kotlin/com/example/controller/InventoryController.kt
@@ -1,12 +1,23 @@
 package com.example.controller
+import com.example.model.Inventory
+import com.example.model.Message
 import com.example.model.Wallet
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.annotation.Body
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.PathVariable
+import io.micronaut.http.annotation.Post
+import io.micronaut.json.tree.JsonObject
 
-class InventoryController(val username:String){
-    var free:Int = 0
-        get() = field
-        set(value) {field = value}
 
-    var locked:Int = 0
-        get() = field
-        set(value) {field = value}
+var inventorMap = HashMap<String,Inventory>()
+
+@Controller("/user")
+class InventoryController(){
+    @Post("/{username}/inventory")
+    fun wallet(@PathVariable username: String, @Body body: JsonObject): HttpResponse<Message> {
+        val quantityToAdd=body["quantity"].intValue
+        inventorMap[username]!!.freeESOP+=quantityToAdd
+        return HttpResponse.ok(Message("{quantityToAdd} ESOPS added to account"))
+    }
 }
---
+++ maverics/src/main/kotlin/com/example/model/Message.kt
@@ -0,0 +1,5 @@
+package com.example.model
+
+class Message(msg :String) {
+    var message=""
+}
