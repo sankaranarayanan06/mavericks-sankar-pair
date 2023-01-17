717f0fe87b165e4c9c393fdd9f89fafcacc01ea0
Amit Bhagat <amitb@Amits-MacBook-Pro.local>
2023-01-17 13:13:57 +0530

    update wallet model

--- maverics/src/main/kotlin/com/example/model/Wallet.kt
+++ maverics/src/main/kotlin/com/example/model/Wallet.kt
@@ -1,6 +1,11 @@
 package com.example.model
 
-class Wallet {
-    var freeAmount:String=""
-    var lockAmount:String=""
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.PathVariable
+
+
+class Wallet(val username:String, var freeAmount:Int = 0, var lockedAmount:Int = 0) {
+
+
 }
