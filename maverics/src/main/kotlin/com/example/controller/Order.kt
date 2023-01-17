ed3650c1bd318eee129ab92e173c4839530fe7a8
Vishal Borse <vishalb@Vishals-MacBook-Pro.local>
2023-01-17 12:35:51 +0530

    added ordered controller with creating a transaction

--- maverics/src/main/kotlin/com/example/controller/Order.kt
+++
@@ -1,7 +0,0 @@
-package com.example.controller
-
-import com.example.model.Order
-
-
-class Order {
-}
--- maverics/src/main/kotlin/com/example/controller/OrderController.kt
+++ maverics/src/main/kotlin/com/example/controller/OrderController.kt
@@ -4,32 +4,178 @@ import com.example.model.Order
 import com.example.model.User
 import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.PathVariable
 import io.micronaut.http.annotation.Post
 import io.micronaut.json.tree.JsonObject
+import java.lang.Integer.min
 
 var orderList= mutableListOf<Order>()
+var orderID=0;
+
+var transactions: MutableMap<Int,MutableList<Pair<Int,Int>>> =  mutableMapOf()
+
 
 @Controller("/user")
 class OrderController {
 
-    @Post("/register")
-    fun register(@Body body: JsonObject): Order {
-
-        var order :
+    @Post("/{username}/order")
+    fun register(@Body body: JsonObject,@PathVariable username:String): Order {
 
 
+        var currentOrder = Order()
+
+        currentOrder.orderId= orderID
+        orderID++
+        currentOrder.quantity = body["quantity"].intValue
+        currentOrder.type = body["type"].stringValue
+        currentOrder.price = body["price"].intValue
+        currentOrder.status = "unfilled"
+        currentOrder.userName=username
 
 
-        val firstName:String = body["firstName"].stringValue
-        val lastName:String = body["lastName"].stringValue
-        val phoneNumber:String = body["phoneNumber"].stringValue
-        val email:String = body["email"].stringValue
-        val username:String = body["username"].stringValue
+        orderList.add(currentOrder)
 
 
-        // check all fields
-        // check if phonenumber, email and username are unique.
-        return newUser
+
+        var n= orderList.size
+
+
+
+
+        if(currentOrder.type=="BUY") {
+
+
+            while(true) {
+
+                if(currentOrder.quantity==0)
+                    break;
+
+                var minSellerPrice = -1;
+                var orderID = -1;
+
+
+
+                for (orderNumber in 0..n - 2) {
+
+
+                    var orderPrev = orderList[orderNumber]
+
+                    if ((orderPrev.status != "filled") && (currentOrder.type != orderPrev.type)) {
+
+                        if (orderPrev.price < minSellerPrice) {
+                            minSellerPrice = orderPrev.price
+                            orderID = orderPrev.orderId
+                        }
+                    }
+                }
+
+                if (orderID != -1) {
+
+
+                    var transQuantity=min(orderList[orderID].quantity,currentOrder.quantity)
+
+                    orderList[orderID].quantity-=transQuantity
+                    currentOrder.quantity-=transQuantity
+
+                    var tmpList : MutableList<Pair<Int,Int>> = mutableListOf()
+                    if(!transactions.containsKey(currentOrder.orderId))
+                    {
+                        transactions.put(currentOrder.orderId,tmpList)
+                    }
+
+                    tmpList= transactions.get(currentOrder.orderId)!!
+
+                    tmpList.add(Pair(transQuantity,minSellerPrice))
+
+
+                    currentOrder.status="partially filled"
+                    orderList[orderID].status="partially filled"
+
+
+                    if(currentOrder.quantity==0)
+                        currentOrder.status="filled"
+                    if(orderList[orderID].quantity==0)
+                        orderList[orderID].status="filled"
+
+                }
+                else
+                    break;
+            }
+
+        }
+        else
+        {
+
+            while(true) {
+
+                if(currentOrder.quantity==0)
+                    break;
+
+                var minSellerPrice = -1;
+                var orderID = -1;
+
+
+
+                for (orderNumber in 0..n - 2) {
+
+
+                    var orderPrev = orderList[orderNumber]
+
+                    if ((orderPrev.status != "filled") && (currentOrder.type != orderPrev.type)) {
+
+                        if (orderPrev.price > minSellerPrice) {
+                            minSellerPrice = orderPrev.price
+                            orderID = orderPrev.orderId
+                        }
+                    }
+                }
+
+                if (orderID != -1) {
+
+
+                    var transQuantity=min(orderList[orderID].quantity,currentOrder.quantity)
+
+                    orderList[orderID].quantity-=transQuantity
+                    currentOrder.quantity-=transQuantity
+
+                    var tmpList : MutableList<Pair<Int,Int>> = mutableListOf()
+                    if(!transactions.containsKey(currentOrder.orderId))
+                    {
+                        transactions.put(currentOrder.orderId,tmpList)
+                    }
+
+                    tmpList= transactions.get(currentOrder.orderId)!!
+
+                    tmpList.add(Pair(transQuantity,minSellerPrice))
+
+
+                    currentOrder.status="partially filled"
+                    orderList[orderID].status="partially filled"
+
+
+                    if(currentOrder.quantity==0)
+                        currentOrder.status="filled"
+                    if(orderList[orderID].quantity==0)
+                        orderList[orderID].status="filled"
+
+                }
+                else
+                    break;
+            }
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+        return currentOrder
     }
 
 
