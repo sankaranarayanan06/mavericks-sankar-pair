f0f6a684d0f67c961ec6b342d44a3ce3adb87028
Pranav-Bhosale <pranav.b.bhosale@gmail.com>
2023-01-17 14:19:48 +0530

    Add User Validation in Inventory Controller

--- maverics/src/main/kotlin/com/example/controller/InventoryController.kt
+++ maverics/src/main/kotlin/com/example/controller/InventoryController.kt
@@ -2,6 +2,7 @@ package com.example.controller
 import com.example.model.Inventory
 import com.example.model.Message
 import com.example.model.Wallet
+import com.example.validations.UserValidation
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
@@ -16,8 +17,14 @@ var inventorMap = HashMap<String,Inventory>()
 class InventoryController(){
     @Post("/{username}/inventory")
     fun wallet(@PathVariable username: String, @Body body: JsonObject): HttpResponse<Message> {
-        val quantityToAdd=body["quantity"].intValue
-        inventorMap[username]!!.freeESOP+=quantityToAdd
-        return HttpResponse.ok(Message("{quantityToAdd} ESOPS added to account"))
+        if(UserValidation.isUserExist(username)) {
+            val quantityToAdd = body["quantity"].intValue
+            inventorMap[username]!!.freeESOP += quantityToAdd
+            return HttpResponse.ok(Message("{quantityToAdd} ESOPS added to account"))
+        }
+        else
+        {
+            return HttpResponse.ok(Message("User not exist"))
+        }
     }
 }
