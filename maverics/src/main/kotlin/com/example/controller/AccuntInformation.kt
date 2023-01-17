a82fee569c0ada544abee231e8927ba007d3ccbf
KCSPreethi <122798707+KCSPreethi@users.noreply.github.com>
2023-01-17 17:52:51 +0530

    Display Account information

---
+++ maverics/src/main/kotlin/com/example/controller/AccuntInformation.kt
@@ -0,0 +1,53 @@
+package com.example.controller
+
+import com.example.model.Accountinfo
+import com.example.model.Message
+import com.example.model.Wallet
+import com.example.model.allUsers
+import com.example.validations.UserValidation
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.PathVariable
+import io.micronaut.http.annotation.Post
+
+@Controller("/user")
+class AccuntInformation {
+
+    @Get("/{username}/accountInformation")
+    fun accountInformation(@PathVariable username: String): HttpResponse<*> {
+        if(UserValidation.isUserExist(username)) {
+            var user = allUsers.get(username);
+            var userWallet = walletList.get(username);
+            var userInventory = inventorMap.get(username);
+
+            var response = mutableMapOf<String, Any>()
+            var walletInfo = mutableMapOf<String, Int>()
+            var inventoryInfo = mutableMapOf<String, Int>()
+
+            walletInfo["free"] = userWallet!!.freeAmount
+            walletInfo["locked"] = userWallet!!.lockedAmount
+
+            inventoryInfo["free"] = userInventory!!.freeESOP
+            inventoryInfo["locked"] = userInventory!!.lockESOP
+
+
+
+            response["firstName"] = user!!.firstName
+            response["lastName"] = user!!.lastName
+            response["phoneNumber"] = user!!.phoneNumber
+            response["email"] = user!!.email
+            
+            response["wallet"] = walletInfo
+            response["inventory"] = inventoryInfo
+
+            return HttpResponse.ok(response)
+
+        }
+        else
+        {
+            return HttpResponse.badRequest(Message("User not exist"))
+        }
+
+
+    }}
