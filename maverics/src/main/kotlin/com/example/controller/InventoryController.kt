fc1fb83c1b6e6455c9cbafded76728db919d2922
Pranav-Bhosale <pranav.b.bhosale@gmail.com>
2023-01-17 11:24:08 +0530

    resolve merge conflict

--- maverics/src/main/kotlin/com/example/controller/InventoryController.kt
+++ maverics/src/main/kotlin/com/example/controller/InventoryController.kt
@@ -1,14 +1,10 @@
 package com.example.controller
-
-<<<<<<< HEAD:maverics/src/main/kotlin/com/example/controller/InventoryController.kt
-class InventoryController(val username:String){
-=======
 import com.example.model.Wallet
 
-var walletMap= HashMap<String,Wallet>()
+var walletMap= HashMap<String, InventoryController.Wallet>()
+class InventoryController(val username:String){
 
 class Wallet(val user_id:Int){
->>>>>>> e977f44 (Update data Structures for controllers):maverics/src/main/kotlin/com/example/controller/Wallet.kt
     var free:Int = 0
         get() = field
         set(value) {field = value}
--- maverics/src/main/kotlin/com/example/controller/Order.kt
+++ maverics/src/main/kotlin/com/example/controller/Order.kt
@@ -2,6 +2,6 @@ package com.example.controller
 
 import com.example.model.Order
 
-var orderList = mutableListOf<Order>()
+
 class Order {
 }
