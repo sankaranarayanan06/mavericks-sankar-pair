6e39c95d0df77567f630f33f8b5af0aecd1c88c1
Amit Bhagat <amitb@Amits-MacBook-Pro.local>
2023-01-17 11:50:41 +0530

    create wallet route

--- maverics/src/main/kotlin/com/example/controller/WalletController.kt
+++ maverics/src/main/kotlin/com/example/controller/WalletController.kt
@@ -1,11 +1,28 @@
 package com.example.controller
 
-class WalletController(val username:String){
-    var free:Int = 0
-        get() = field
-        set(value) {field = value}
+import com.example.model.Order
+import com.example.model.Wallet
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.annotation.Body
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.PathVariable
+import io.micronaut.http.annotation.Post
+import io.micronaut.json.tree.JsonObject
 
-    var locked:Int = 0
-        get() = field
-        set(value) {field = value}
+var walletList = mutableMapOf<String,Wallet>()
+
+@Controller("/user")
+class WalletController(){
+    @Post("/{username}/wallet")
+    fun wallet(@PathVariable username: String, @Body body: JsonObject): HttpResponse<Wallet> {
+        val amount:Int = body["amount"].intValue
+        if(!walletList.containsKey(username)){
+            val newWallet:Wallet = Wallet(username)
+            walletList.put(username,newWallet)
+        }
+        val wallet: Wallet = walletList.get(username)!!
+        wallet.freeAmount += amount
+        return HttpResponse.ok(wallet)
+    }
 }
