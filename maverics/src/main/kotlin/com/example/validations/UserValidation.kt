59fc4be413ec6b7b164e5d199c815fe29d1b4d62
dnyaneshwar2023 <dnyaneshwarw@sahaj.ai>
2023-01-23 11:33:13 +0530

    Update first name validation based on length

--- maverics/src/main/kotlin/com/example/validations/UserValidation.kt
+++ maverics/src/main/kotlin/com/example/validations/UserValidation.kt
@@ -3,32 +3,48 @@ package com.example.validations
 import com.example.model.allUsers
 
 class UserValidation {
-    fun ifUniqueUsername(username: String): Boolean {
-        return !allUsers.containsKey(username)
-    }
-
-    fun ifUniqueEmail(email: String): Boolean {
-        for(user in allUsers.keys) {
-            if (allUsers[user]?.email == email) {
-                return false
-            }
-        }
-        return true
-    }
-
-    fun ifUniquePhoneNumber(phoneNumber: String): Boolean {
-        for(user in allUsers.keys) {
-            if (allUsers[user]?.phoneNumber == phoneNumber) {
-                return false
-            }
-        }
-        return true
-    }
-
     companion object {
+        fun ifUniquePhoneNumber(phoneNumber: String): Boolean {
+            for (user in allUsers.keys) {
+                if (allUsers[user]?.phoneNumber == phoneNumber) {
+                    return false
+                }
+            }
+            return true
+        }
+
         fun isUserExist(username: String): Boolean {
             return allUsers.containsKey(username)
         }
-    }
 
+        fun ifUniqueEmail(email: String): Boolean {
+            for (user in allUsers.keys) {
+                if (allUsers[user]?.email == email) {
+                    return false
+                }
+            }
+            return true
+        }
+
+
+        fun checkFirstName(firstName: String): List<String> {
+            return checkFirstNameExists(firstName) + checkFirstNameNotTooBig(firstName)
+        }
+
+        private fun checkFirstNameNotTooBig(firstName: String): List<String> {
+            return if(firstName.length > 50) {
+                listOf("First name should be less than 50 characters")
+            } else listOf()
+        }
+
+        private fun checkFirstNameExists(firstName: String): List<String> {
+            return if(firstName.isEmpty()) {
+                listOf("Username cannot be empty")
+            } else listOf()
+        }
+
+        fun ifUniqueUsername(username: String): Boolean {
+            return !allUsers.containsKey(username)
+        }
+    }
 }
