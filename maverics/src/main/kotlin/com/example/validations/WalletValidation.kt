63fcb9e3ef7433578245d007251605fb434b87c2
dnyaneshwar2023 <122769564+dnyaneshwar2023@users.noreply.github.com>
2023-01-31 11:32:51 +0530

    Add wallet error response changes

--- maverics/src/main/kotlin/com/example/validations/WalletValidation.kt
+++ maverics/src/main/kotlin/com/example/validations/WalletValidation.kt
@@ -1,17 +1,15 @@
-package com.example.validations;
-
+package com.example.validations
 import com.example.constants.Amounts
-
 
 class WalletValidation {
     fun validations(walletAmount: Long, userAmount: Long): MutableList<String>{
-        val walleterror = mutableListOf<String>()
+        val walletError = mutableListOf<String>()
         if (userAmount !in 1..Amounts.MAX_WALLET_AMOUNT) {
-            walleterror.add("Amount out of Range. Max: ${Amounts.MAX_WALLET_AMOUNT}, Min: 1")
+            walletError.add("Amount out of Range Maximum: ${Amounts.MAX_WALLET_AMOUNT}, Minimum: 1")
         }
         if(walletAmount+userAmount > Amounts.MAX_WALLET_AMOUNT){
-            walleterror.add("Max wallet limit of ${Amounts.MAX_WALLET_AMOUNT} would be exceeded.")
+            walletError.add("Maximum wallet limit of amount ${Amounts.MAX_WALLET_AMOUNT} would be exceeded")
         }
-        return walleterror
+        return walletError
     }
 }
