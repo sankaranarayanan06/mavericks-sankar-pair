b8648fea28c26331a25b024cf19a306be3b751c4
dnyaneshwar2023 <122769564+dnyaneshwar2023@users.noreply.github.com>
2023-02-01 12:41:33 +0530

    Fix validation warnings

--- maverics/src/main/kotlin/com/example/validations/user/UserValidation.kt
+++ maverics/src/main/kotlin/com/example/validations/user/UserValidation.kt
@@ -4,47 +4,6 @@ import com.example.constants.allUsers
 
 class UserValidation {
     companion object {
-        fun ifUniquePhoneNumber(phoneNumber: String): Boolean {
-            for (user in allUsers.keys) {
-                if (allUsers[user]?.phoneNumber == phoneNumber) {
-                    return false
-                }
-            }
-            return true
-        }
 
-        fun isUserExist(username: String): Boolean {
-            return allUsers.containsKey(username)
-        }
-
-        fun ifUniqueEmail(email: String): Boolean {
-            for (user in allUsers.keys) {
-                if (allUsers[user]?.email == email) {
-                    return false
-                }
-            }
-            return true
-        }
-
-
-        fun checkFirstName(firstName: String): List<String> {
-            return checkFirstNameExists(firstName) + checkFirstNameNotTooBig(firstName)
-        }
-
-        private fun checkFirstNameNotTooBig(firstName: String): List<String> {
-            return if(firstName.length > 50) {
-                listOf("First name should be less than 50 characters")
-            } else listOf()
-        }
-
-        private fun checkFirstNameExists(firstName: String): List<String> {
-            return if(firstName.isEmpty()) {
-                listOf("First Name cannot be empty")
-            } else listOf()
-        }
-
-        fun ifUniqueUsername(username: String): Boolean {
-            return !allUsers.containsKey(username)
-        }
     }
 }
