9bb3afc6cefa4c39c579661c4058e1e482a8557b
dnyaneshwar2023 <dnyaneshwarw@sahaj.ai>
2023-02-01 16:05:30 +0530

    Update data type to BigInteger

--- maverics/src/main/kotlin/com/example/model/Inventory.kt
+++ maverics/src/main/kotlin/com/example/model/Inventory.kt
@@ -1,3 +1,9 @@
 package com.example.model
 
-class Inventory(var free: Long = 0, var locked: Long = 0, var type: String = "PERFORMANCE")
+import java.math.BigInteger
+
+class Inventory(
+    var free: BigInteger = BigInteger.ZERO,
+    var locked: BigInteger = BigInteger.ZERO,
+    var type: String = "PERFORMANCE"
+)
--- maverics/src/main/kotlin/com/example/services/InventoryHandler.kt
+++ maverics/src/main/kotlin/com/example/services/InventoryHandler.kt
@@ -1,26 +1,51 @@
 package com.example.services
 
+import com.example.constants.Limits
 import com.example.constants.inventoryData
 import com.example.model.Inventory
+import java.math.BigInteger
 
 class InventoryHandler {
     companion object {
-        fun addToNonPerformanceInventory(quantity: Long, userName: String) {
-            inventoryData[userName]!![0].free += quantity
-        }
-
-        fun addToPerformanceInventory(quantity: Long, userName: String) {
+        fun addToNonPerformanceInventory(quantity: BigInteger, userName: String): Boolean {
+            if(inventoryData[userName]!![1].free + quantity + quantity > Limits.MAX_INVENTORY_QUANTITY){
+                return false
+            }
             inventoryData[userName]!![1].free += quantity
+            return true
         }
 
-        fun lockNoNPerformanceInventory(quantity: Long, userName: String) {
-            inventoryData[userName]!![0].locked += quantity
+        fun addToPerformanceInventory(quantity: BigInteger, userName: String): Boolean {
+            if(inventoryData[userName]!![0].free + quantity + quantity > Limits.MAX_INVENTORY_QUANTITY){
+                return false
+            }
+            inventoryData[userName]!![0].free += quantity
+            return true
         }
 
-        fun lockPerformanceInventory(quantity: Long, userName: String) {
+        fun lockNonPerformanceInventory(quantity: BigInteger, userName: String) {
             inventoryData[userName]!![1].locked += quantity
         }
 
+        fun lockPerformanceInventory(quantity: BigInteger, userName: String) {
+            inventoryData[userName]!![0].locked += quantity
+        }
+
+        fun getFreePerformanceInventory(userName: String): BigInteger {
+            return inventoryData[userName]!![0].free
+        }
+
+        fun getFreeNonPerformanceInventory(userName: String): BigInteger {
+            return inventoryData[userName]!![1].free
+        }
+
+        fun getLockedPerformanceInventory(userName: String): BigInteger {
+            return inventoryData[userName]!![0].locked
+        }
+
+        fun getLockedNonPerformanceInventory(userName: String): BigInteger {
+            return inventoryData[userName]!![1].locked
+        }
         fun getInventoryInfo(username: String): MutableList<Inventory> {
             val inventoryInfo: MutableList<Inventory> = mutableListOf(Inventory(type = "PERFORMANCE"), Inventory(type = "NON_PERFORMANCE"))
             inventoryInfo[0] = inventoryData[username]?.get(0)!!
