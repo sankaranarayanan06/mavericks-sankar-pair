4a4bb1b6ee8d4ef206fbd5dd3ea22f910ef4f1e8
dnyaneshwar2023 <122769564+dnyaneshwar2023@users.noreply.github.com>
2023-02-01 11:57:43 +0530

    Update limits and limits controller

---
+++ maverics/src/main/kotlin/com/example/validations/LimitsValidation.kt
@@ -0,0 +1,47 @@
+package com.example.validations
+
+import com.example.constants.Limits
+import io.micronaut.http.HttpResponse
+import io.micronaut.json.tree.JsonObject
+
+var limitsError: MutableList<String> = mutableListOf()
+
+class LimitsValidation {
+    companion object {
+        fun validateLimits(body: JsonObject): HttpResponse<*> {
+            if (body["maxOrderPrice"] == null && body["maxOrderQuantity"] == null && body["maxInventoryPrice"] == null && body["maxInventoryQuantity"] == null) {
+                limitsError.add("Invalid Key Entered. Valid Keys: [maxOrderPrice, maxOrderQuantity, maxWalletAmount, maxInventoryQuantity]")
+                val response = mutableMapOf<String, MutableList<String>>();
+
+                response["errors"] = limitsError
+                return HttpResponse.ok(response)
+            }
+
+            if(body["maxOrderPrice"] != null) {
+                limitsError.add("maxOrderPrice")
+                Limits.setMaxOrderPrice(body["maxOrderPrice"].bigIntegerValue)
+            }
+
+            if(body["maxOrderQuantity"] != null) {
+                limitsError.add("maxOrderQuantity")
+                Limits.setMaxOrderPrice(body["maxOrderQuantity"].bigIntegerValue)
+            }
+
+            if(body["maxInventoryQuantity"] != null) {
+                limitsError.add("maxInventoryQuantity")
+                Limits.setMaxOrderPrice(body["maxInventoryQuantity"].bigIntegerValue)
+            }
+
+            if(body["maxWalletAmount"] != null) {
+                limitsError.add("maxWalletAmount")
+                Limits.setMaxOrderPrice(body["maxWalletAmount"].bigIntegerValue)
+            }
+
+            val response = mutableMapOf<String, MutableList<String>>();
+
+            response["updates"] = limitsError
+
+            return HttpResponse.ok(response)
+        }
+    }
+}
--- maverics/src/main/kotlin/com/example/validations/WalletValidation.kt
+++ maverics/src/main/kotlin/com/example/validations/WalletValidation.kt
@@ -1,14 +1,16 @@
 package com.example.validations
-import com.example.constants.Amounts
+
+import com.example.constants.Limits
+import java.math.BigInteger
 
 class WalletValidation {
-    fun validations(walletAmount: Long, userAmount: Long): MutableList<String>{
+    fun validations(walletAmount: Long, userAmount: Long): MutableList<String> {
         val walletError = mutableListOf<String>()
-        if (userAmount !in 1..Amounts.MAX_WALLET_AMOUNT) {
-            walletError.add("Amount out of Range Maximum: ${Amounts.MAX_WALLET_AMOUNT}, Minimum: 1")
+        if (BigInteger.valueOf(userAmount) !in BigInteger.ONE..Limits.MAX_WALLET_AMOUNT) {
+            walletError.add("Amount out of Range Maximum: ${Limits.MAX_WALLET_AMOUNT}, Minimum: 1")
         }
-        if(walletAmount+userAmount > Amounts.MAX_WALLET_AMOUNT){
-            walletError.add("Maximum wallet limit of amount ${Amounts.MAX_WALLET_AMOUNT} would be exceeded")
+        if (BigInteger.valueOf(walletAmount + userAmount) > Limits.MAX_WALLET_AMOUNT) {
+            walletError.add("Maximum wallet limit of amount ${Limits.MAX_WALLET_AMOUNT} would be exceeded")
         }
         return walletError
     }
