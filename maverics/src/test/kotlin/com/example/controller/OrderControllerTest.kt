54512d4eb5e25f2ead6443fb32529571d3196db9
dnyaneshwar2023 <dnyaneshwarw@sahaj.ai>
2023-02-03 09:27:39 +0530

    Update order controller tests

--- maverics/src/test/kotlin/com/example/controller/OrderControllerTest.kt
+++ maverics/src/test/kotlin/com/example/controller/OrderControllerTest.kt
@@ -3,10 +3,8 @@ package com.example.controller
 import com.example.constants.allUsers
 import com.example.constants.inventoryData
 import com.example.constants.orderList
-import com.example.model.Inventory
-import com.example.model.Order
-import com.example.model.OrderRequest
-import com.example.model.User
+import com.example.model.*
+import com.example.services.InventoryHandler
 import com.example.services.addUser
 import com.fasterxml.jackson.databind.ObjectMapper
 import io.micronaut.http.HttpRequest
@@ -15,7 +13,6 @@ import io.micronaut.http.client.annotation.Client
 import io.micronaut.http.client.exceptions.HttpClientResponseException
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
 import jakarta.inject.Inject
-import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -37,6 +34,13 @@ class OrderControllerTest {
         email = "as",
         phoneNumber = "4234234"
     )
+    val seller = User(
+        firstName = "Dnyaneshwar",
+        lastName = "Ware",
+        userName = "dnyaneshwar",
+        email = "as",
+        phoneNumber = "4234234"
+    )
 
     @BeforeEach
     fun clearData() {
@@ -44,6 +48,33 @@ class OrderControllerTest {
         orderList.clear()
 
         addUser(buyer)
+        addUser(seller)
+    }
+
+    @Test
+    fun `Place a valid sell order`(objectMapper: ObjectMapper) {
+        // Arrange
+        inventoryData[buyer.userName] =
+            mutableListOf(Inventory(BigInteger.ZERO, BigInteger.ZERO), Inventory(BigInteger.ZERO, BigInteger.ZERO))
+
+        InventoryHandler.addToNonPerformanceInventory(BigInteger.valueOf(100), seller.userName)
+
+        val sellOrder = OrderRequest("SELL", BigInteger.TEN, BigInteger.TEN, esopType = "NON_PERFORMANCE")
+        val expected = Order(price = sellOrder.price, type = sellOrder.type, orderId = 1, placedQuantity = BigInteger.TEN, currentQuantity = BigInteger.TEN)
+        // Act
+        val request = HttpRequest.POST(
+            "/user/${buyer.userName}/order", objectMapper.writeValueAsString(sellOrder)
+        )
+
+        // Assert
+        val response = client.toBlocking().retrieve(request)
+       // "{\"quantity\":10,\"orderId\":1,\"price\":10,\"userName\":\"dnyaneshwar\",\"type\":\"BUY\"}"
+
+
+        assertEquals(
+            objectMapper.writeValueAsString(expected),
+            response
+        )
     }
 
     @Test
@@ -52,8 +83,11 @@ class OrderControllerTest {
         inventoryData[buyer.userName] =
             mutableListOf(Inventory(BigInteger.ZERO, BigInteger.ZERO), Inventory(BigInteger.ZERO, BigInteger.ZERO))
         walletList[buyer.userName]!!.freeAmount = BigInteger.valueOf(200)
+
+
         val buyOrder = OrderRequest("BUY", BigInteger.TEN, BigInteger.TEN)
 
+        val buyOrderResponse = BuyOrderResponse(BigInteger.TEN,BigInteger.TEN,"unfilled","BUY",1)
         // Act
         val request = HttpRequest.POST(
             "/user/${buyer.userName}/order", objectMapper.writeValueAsString(buyOrder)
@@ -63,8 +97,8 @@ class OrderControllerTest {
         val response = client.toBlocking().retrieve(request)
 
         assertEquals(
-            "{\"quantity\":10,\"orderId\":1,\"price\":10,\"userName\":\"dnyaneshwar\",\"type\":\"BUY\"}",
-            response
+            buyOrderResponse,
+            objectMapper.readValue(response,BuyOrderResponse::class.java)
         )
 
 
@@ -90,7 +124,6 @@ class OrderControllerTest {
         }
 
         println(exception.message)
-
 
 
     }
