6e999a1ba01595900857e3d899844f4b2e9909e0
sankar <sankaranarayananm@sahaj.ai>
2023-02-08 16:04:04 +0530

    Add sell order

--- src/test/kotlin/com/example/services/OrderServiceTest.kt
+++ src/test/kotlin/com/example/services/OrderServiceTest.kt
@@ -2,6 +2,7 @@ package com.example.services
 
 import com.example.model.BuyOrderResponse
 import com.example.model.Order
+import com.example.model.SellOrderResponse
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import java.math.BigInteger
@@ -12,10 +13,21 @@ class OrderServiceTest{
     @Test
     fun `it should place a buy order`(){
         createUser("anushka","joshi","1234567890","aushka@sahaj.ai","anushka")
-        val order = Order(BigInteger.valueOf(10),BigInteger.valueOf(10),"BUY","anushka")
+        val order = Order(BigInteger.valueOf(10),BigInteger.valueOf(10),"BUY","anushka","")
         val expectedResponse = BuyOrderResponse(BigInteger.valueOf(10),BigInteger.valueOf(10),"unfilled","BUY",1)
 
         val response = orderService.placeBuyOrder(order)
+
+        assertEquals(expectedResponse,response["orderDetails"])
+    }
+
+    @Test
+    fun `it should place a sell order`(){
+        createUser("sankar","m","1234678904","sankar@sahaj.ai","sankar")
+        val order = Order(BigInteger.valueOf(10),BigInteger.valueOf(10),"SELL","sankar","PERFORMANCE")
+        val expectedResponse = SellOrderResponse(1,BigInteger.valueOf(10),BigInteger.valueOf(10),"unfilled","SELL","PERFORMANCE")
+
+        val response = orderService.placeSellOrder(order)
 
         assertEquals(expectedResponse,response["orderDetails"])
     }
