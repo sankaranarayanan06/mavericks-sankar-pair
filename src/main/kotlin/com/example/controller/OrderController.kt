3ebccfdb643d402a7130a020ac0e353e28a4f4ef
sankar <sankaranarayananm@sahaj.ai>
2023-02-08 21:06:19 +0530

    Refactor: accept request through OrderDto + response body changed to Response body type

--- src/main/kotlin/com/example/controller/OrderController.kt
+++ src/main/kotlin/com/example/controller/OrderController.kt
@@ -1,82 +1,42 @@
 package com.example.controller
 
 import com.example.constants.*
+import com.example.dto.OrderDTO
 import com.example.model.Order
 import com.example.model.OrderResponse
+import com.example.model.ResponseBody
 import com.example.services.*
 import com.example.validations.isUserExists
-import com.example.validations.order.ifSufficientAmountInWallet
-import com.example.validations.order.orderValidation
-import com.example.validations.order.orderoverflowValidation
+import com.example.validations.order.validateOrder
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.PathVariable
 import io.micronaut.http.annotation.Post
-import io.micronaut.json.tree.JsonObject
 
 
 @Controller("/user")
-class OrderController {
+class OrderController() {
+
+    var orderService =  OrderService()
     @Post("/{username}/order")
-    fun addNewOrder(@Body body: JsonObject, @PathVariable username: String): HttpResponse<*> {
+    fun addNewOrder(@Body body:OrderDTO, @PathVariable username: String): HttpResponse<ResponseBody> {
         if (!isUserExists(username)) {
-            return generateErrorResponse(mutableListOf("User does not exist"))
+            return HttpResponse
+                .badRequest(ResponseBody(listOf("User does not exist."),null))
         }
 
-        val currentOrder = getOrderFromBody(body, username)
-
+        val currentOrder = Order(body.price,body.quantity,body.type,username)
         val errors = validateOrder(currentOrder, username)
         if (errors.size > 0) {
-            return generateErrorResponse(errors)
+            return HttpResponse.badRequest(ResponseBody(errors,null))
         }
 
-        val response: MutableMap<String, OrderResponse>
-
-        val orderService = OrderService()
-        if (currentOrder.type == "BUY") {
-            response = orderService.placeBuyOrder(currentOrder)
-        } else{
-            if (body["esopType"]!!.stringValue == "PERFORMANCE") {
-                currentOrder.esopType = "PERFORMANCE"
-            }
-            response = orderService.placeSellOrder(currentOrder)
+        val response: OrderResponse = if (currentOrder.type == "BUY") {
+            orderService.placeBuyOrder(body,username)
+        } else {
+            orderService.placeSellOrder(body, username)
         }
-        return HttpResponse.ok(response)
-    }
-
-    private fun validateOrder(
-        currentOrder: Order,
-        username: String
-    ): MutableList<String> {
-        val errorList = mutableListOf<String>()
-        orderValidation(
-            errorList,
-            currentOrder.placedQuantity,
-            currentOrder.type,
-            currentOrder.price
-        )
-
-        orderoverflowValidation(
-            errorList,
-            username,
-            currentOrder.placedQuantity,
-            currentOrder.price,
-            currentOrder.type
-        )
-
-        if (!ifSufficientAmountInWallet(username, currentOrder.currentQuantity * currentOrder.price)) {
-            errorList.add("Insufficient amount in wallet")
-        }
-        return errorList
-    }
-
-    private fun getOrderFromBody(body: JsonObject, username: String): Order {
-        return Order(
-            body["price"]!!.bigIntegerValue,
-            body["quantity"]!!.bigIntegerValue,
-            body["type"]!!.stringValue,
-            userName = username
-        )
+        return HttpResponse.ok(ResponseBody(null,response))
     }
 }
