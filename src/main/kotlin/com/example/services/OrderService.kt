667483dc4734eb26cbc05cdb7265dd40a43d6863
sankar <sankaranarayananm@sahaj.ai>
2023-02-08 16:04:04 +0530

    Add sell order

--- src/main/kotlin/com/example/services/OrderService.kt
+++ src/main/kotlin/com/example/services/OrderService.kt
@@ -1,9 +1,11 @@
 package com.example.services
 
+import com.example.constants.inventoryData
 import com.example.constants.orderList
 import com.example.constants.transactions
 import com.example.model.BuyOrderResponse
 import com.example.model.Order
+import com.example.model.SellOrderResponse
 
 class OrderService {
     fun placeBuyOrder(order: Order): MutableMap<String, BuyOrderResponse> {
@@ -25,5 +27,39 @@ class OrderService {
         print(result)
         return result
 
+    }
+    fun placeSellOrder(order: Order): MutableMap<String,SellOrderResponse> {
+        val username = order.userName
+        val result = mutableMapOf<String, SellOrderResponse>()
+
+        mutableListOf<String>()
+
+
+
+        orderList[order.orderId] = order
+        transactions[order.orderId] = mutableListOf()
+
+        result["orderDetails"] = SellOrderResponse(order)
+
+        // Locking
+        when (order.esopType) {
+            "PERFORMANCE" -> {
+                inventoryData[order.userName]!![0].free -= order.currentQuantity
+                inventoryData[order.userName]!![0].locked += order.currentQuantity
+
+                performSells(order, username)
+            }
+
+            "NON_PERFORMANCE" -> {
+                inventoryData[order.userName]!![1].free -= order.currentQuantity
+                inventoryData[order.userName]!![1].locked += order.currentQuantity
+
+                performSells(order, username)
+
+            }
+
+
+        }
+        return result
     }
 }
