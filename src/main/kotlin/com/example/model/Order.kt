67a926cc29b94adf9951419f01ef909dc7f7e244
sankar <sankaranarayananm@sahaj.ai>
2023-02-08 15:25:48 +0530

    Modify order class

--- src/main/kotlin/com/example/model/Order.kt
+++ src/main/kotlin/com/example/model/Order.kt
@@ -4,14 +4,14 @@ import java.math.BigInteger
 
 class Order(
     var price: BigInteger = BigInteger.ZERO,
-    var currentQuantity: BigInteger = BigInteger.ZERO,
     var placedQuantity: BigInteger = BigInteger.ZERO,
-    var status: String = "unfilled",
     var type: String = "",
-    var esopType: String = "NON_PERFORMANCE",
-    var userName: String = "",
-    val orderId: Int = orderIdCounter++
+    var userName: String = ""
 ) {
+    var currentQuantity: BigInteger = BigInteger.ZERO
+    var status: String = "unfilled"
+    var esopType = if(type == "SELL") "NON_PERFORMANCE" else ""
+    val orderId: Int = orderIdCounter++
 
     companion object {
         var orderIdCounter = 1
