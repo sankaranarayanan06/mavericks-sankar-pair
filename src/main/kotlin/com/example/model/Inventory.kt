915748de823c48c6da14b6b14f76cfcd1010ac16
sankar <sankaranarayananm@sahaj.ai>
2023-02-11 18:58:00 +0530

    Add: EsopType to categorize the type of esop

--- src/main/kotlin/com/example/model/Inventory.kt
+++ src/main/kotlin/com/example/model/Inventory.kt
@@ -1,9 +1,15 @@
 package com.example.model
 
+import com.example.model.EsopType.NON_PERFORMANCE
 import java.math.BigInteger
 
-class Inventory(
+enum class EsopType(val type: String) {
+    NON_PERFORMANCE("NON_PERFORMANCE"),
+    PERFORMANCE("PERFORMANCE")
+}
+
+data class Inventory(
     var free: BigInteger = BigInteger.ZERO,
     var locked: BigInteger = BigInteger.ZERO,
-    var type: String = "PERFORMANCE"
+    var type: EsopType = NON_PERFORMANCE
 )
