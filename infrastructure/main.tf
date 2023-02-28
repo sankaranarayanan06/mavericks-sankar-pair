bf27cad5243b4f57f286fdbc9b24ff0ca9f248ed
sankar <sankaranarayananm@sahaj.ai>
2023-02-28 17:39:00 +0530

    Add: Terraform to create infrastructure

---
+++ infrastructure/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0e742cca61fb65051"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "SankarSampleEsopApp"
+  }
+}
