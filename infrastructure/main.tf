d2fea739148d87f3ed634a2fe347bddaef3eb592
sankar <sankaranarayananm@sahaj.ai>
2023-02-28 17:39:00 +0530

    Add: Terraform to create infrastructure

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -7,17 +7,33 @@ terraform {
   }
 
   required_version = ">= 1.2.0"
+  backend "s3" {
+    bucket = "gurukul-terraform-tutorial"
+    key    = "sankar/terraform.tfstate"
+    region = "ap-south-1"
+  }
 }
 
 provider "aws" {
   region = "ap-south-1"
 }
 
+data "aws_key_pair" "key-pair" {
+  key_name = "gurukul-2023"
+}
+
+data "aws_security_group" "security-group" {
+  vpc_id = "vpc-e4bc548f"
+}
+
 resource "aws_instance" "app_server" {
-  ami           = "ami-0e742cca61fb65051"
-  instance_type = "t2.micro"
+  ami             = "ami-0e742cca61fb65051"
+  instance_type   = "t2.micro"
+  key_name        = data.aws_key_pair.key-pair.key_name
+  security_groups = data.aws_security_group.security-group.vpc_id
 
   tags = {
-    Name = "SankarSampleEsopApp"
+    Name  = "SankarSampleEsopApp"
+    Owner = "Sankar"
   }
 }
